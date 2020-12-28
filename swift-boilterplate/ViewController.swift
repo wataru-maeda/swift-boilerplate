63126336f3f80e01c5621f2565df98650dd302ec
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2020-12-27 22:51:45 -0800

    feat(dev): configure SwiftLint (#1)

---
+++ swift-boilterplate/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  swift-boilterplate
+//
+//  Created by Wataru Maeda on 2020/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
